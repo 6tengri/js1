9147f9cb7dda1980fc834406027653ea6e9827d4
Merey <asylbekmerey05@gmail.com>
2022-06-25 01:46:36 +0500

    Initial commit

---
+++ jsfolder/1.js
@@ -0,0 +1,28 @@
+// ex-exercise
+
+// 1 ex
+document.write("Welcome Home");
+
+// 2 ex
+
+var a = 5 + 3 + 4 + 8 + 5;
+var b = "Hello";
+
+console.log(a, b);
+
+// 3 ex
+let example3 = "123456";
+let example33 = Number.parseInt(example3);
+
+console.log(example33);
+
+// 4 ex
+let example4 = "Я использую косметику от 'Chanel'";
+
+console.log(example4);
+
+// 5 ex
+let user_age = prompt("your age");
+alert(user_age);
+
+console.log(Math.round(user_age));
